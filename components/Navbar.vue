e75c8cffa00959e06c26360310efd7f4336a7227
vivekascoder <vivekascoder@gmail.com>
2021-07-05 22:23:10 +0530

    Fixed Navbar Issues, and created project-config.json

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,11 +1,12 @@
 <template>
   <div
-    class="border-b-2 border-gray-200 shadow-sm fixed bg-white top-0 left-0 right-0 h-16 z-50"
+    class="nav-wrapper"
   >
     <nav
-      class="flex items-center justify-between container mx-auto relative h-full px-4"
+      class="nav"
     >
-      <div class="flex items-center space-x-1">
+    <!-- Blog's Title -->
+      <div class="nav__title">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           class="h-6 w-6"
@@ -22,33 +23,24 @@
         </svg>
         <span class="font-semibold uppercase text-2xl">vivekascoder</span>
       </div>
-      <div class="flex space-x-4 items-center h-full">
+      <div class="nav__links">
         <div
-          class="flex md:space-x-4 md:items-center absolute md:static top-full bg-white md:bg-transparent flex-col md:flex-row left-0 right-0 md:mt-0 shadow-lg md:shadow-none md:border-none border-b-4 border-green-600 rounded-b-lg z-40"
-          :class="{'hidden md:static': showMenu}"
+          class="nav__links__wrapper"
         >
-          <span
-            class="my-1 px-4 py-4 text-center md:text-left md:py-1 hover:bg-gray-800 hover:text-white cursor-pointer md:rounded-full transition duration-100 ease-in-out block "
-            >Home</span
+          <nuxt-link
+            v-for="link in links"
+            :key="link.id"
+            :to="link.path"
+            class="nav__link"
           >
-          <span
-            class="my-1 px-4 py-4 text-center md:text-left md:py-1 hover:bg-gray-800 hover:text-white cursor-pointer md:rounded-full transition duration-100 ease-in-out block "
-            >Projects</span
-          >
-          <span
-            class="my-1 px-4 py-4 text-center md:text-left md:py-1 hover:bg-gray-800 hover:text-white cursor-pointer md:rounded-full transition duration-100 ease-in-out block "
-            >About Me</span
-          >
-          <span
-            class="my-1 px-4 py-4 text-center md:text-left md:py-1 hover:bg-gray-800 hover:text-white cursor-pointer md:rounded-full transition duration-100 ease-in-out block "
-            >Contact</span
-          >
+          {{link.name}}
+          </nuxt-link>
         </div>
 
         <!-- Youtube Btn -->
         <a
-          href="#!"
-          class="flex bg-red-500 items-center text-white px-6 py-2 rounded-full space-x-2 hover:bg-red-600 transition duration-100 ease-in-out"
+          href="https://www.youtube.com/channel/UC1uPwKU2VB9d1COLsyT4_GA"
+          class="btn__yt"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -70,10 +62,11 @@
               d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
             />
           </svg>
-          <span class="font-semibold uppercase text-xs tracking-wider">Watch</span>
+          <span class="btn__yt__title">Watch</span>
         </a>
+        <!-- Mobile Nav toggler Button -->
         <span
-          class="static md:hidden bg-gray-100 p-3 rounded-full cursor-pointer hover:bg-gray-800 hover:text-gray-200 transition duration-100 ease-in-out"
+          class="btn__mobile-nav"
           @click="toggleMenu()"
         >
           <svg
@@ -93,47 +86,72 @@
         </span>
       </div>
     </nav>
+    <!-- Mobile Navbar... -->
+    <div
+      class="nav__mobile flex md:hidden"
+      :class="{'hidden': !showMenu}"
+    >
+      <nuxt-link
+        v-for="link in links"
+        :key="link.id"
+        :to="link.path"
+        class="nav__mobile__link"
+      >
+        {{link.name}}
+      </nuxt-link>
+    </div>
   </div>
 </template>
 
 <script>
+import {links} from '../project-config.json'
 export default {
   data() {
     return {
       showMenu: false,
-      links: [
-        {
-          id: 1,
-          name: "Home",
-          path: "/"
-        },
-        {
-          id: 2,
-          name: "About",
-          path: "/about"
-        },
-        {
-          id: 3,
-          name: "Contact",
-          path: "/"
-        }
-      ]
+      links: links
     };
   },
   methods: {
     toggleMenu() {
       this.showMenu = !this.showMenu;
-      console.log(this.showMenu);
     }
   }
 };
 </script>
 
 <style lang="postcss" scoped>
-.links .link {
-  @apply px-4;
-  &:hover {
-    @apply underline;
-  }
+.nav-wrapper {
+  @apply border-b-2 border-gray-200 shadow-sm fixed bg-white top-0 left-0 right-0 h-16 z-50;
+}
+.nav {
+  @apply flex items-center justify-between container mx-auto relative h-full px-4;
+}
+.nav__title {
+  @apply flex items-center space-x-1;
+}
+.nav__links {
+  @apply flex space-x-4 items-center h-full;
+}
+.nav__links__wrapper {
+  @apply flex md:space-x-4 md:items-center z-40 hidden md:flex;
+}
+.nav__link {
+  @apply my-1 px-4 py-4 text-center md:text-left md:py-1 hover:bg-gray-800 hover:text-white cursor-pointer md:rounded-full transition duration-100 ease-in-out block;
+}
+.btn__yt {
+  @apply flex bg-red-500 items-center text-white px-6 py-2 rounded-full space-x-2 hover:bg-red-600 transition duration-100 ease-in-out;
+}
+.btn__yt__title {
+  @apply font-semibold uppercase text-xs tracking-wider;
+}
+.btn__mobile-nav {
+  @apply static md:hidden bg-gray-100 p-3 rounded-full cursor-pointer hover:bg-gray-800 hover:text-gray-200 transition duration-100 ease-in-out;
+}
+.nav__mobile {
+  @apply absolute top-full bg-white flex-col left-0 right-0 shadow-lg border-b-4 border-green-600 rounded-b-lg z-40;
+}
+.nav__mobile__link {
+  @apply my-1 px-4 py-4 text-center hover:bg-gray-800 hover:text-white cursor-pointer transition duration-100 ease-in-out block;
 }
 </style>
