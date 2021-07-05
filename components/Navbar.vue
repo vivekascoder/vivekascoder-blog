7bf3c9792e300ba9f3532c96e503490a3f5674b6
Ubuntu <vivekascoder@vm.0pz0agk5imcepggmc5naoclczc.rx.internal.cloudapp.net>
2021-07-05 16:14:17 +0000

    Added New Navbar (Slight Issues Needs to addressed)

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,45 +1,11 @@
 <template>
-<!-- 
-  Bare code.
-  <nav class="flex items-center justify-between container mx-auto">
-  <div class="flex items-cente">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-      />
-    </svg>
-    <span class="font-semibold uppercase">vivekascoder</span>
-  </div>
-  <div class="flex space-x-4">
-    <span class="my-4 px-4 py-1 hover:bg-gray-800 hover:text-white cursor-pointer rounded-full">
-      Home
-    </span>
-    <span class="my-4 px-4 py-1 hover:bg-gray-800 hover:text-white cursor-pointer rounded-full">
-      Projects
-    </span>
-    <span class="my-4 px-4 py-1 hover:bg-gray-800 hover:text-white cursor-pointer rounded-full">
-      About Me
-    </span>
-    <span class="my-4 px-4 py-1 hover:bg-gray-800 hover:text-white cursor-pointer rounded-full">
-      Contact
-    </span>
-  </div>
-</nav>
- -->
-  <div class="fixed left-0 top-0 right-0 bg-white z-50 shadow-md">
+  <div
+    class="border-b-2 border-gray-200 shadow-sm fixed bg-white top-0 left-0 right-0 h-16 z-50"
+  >
     <nav
-      class="relative container mx-auto flex items-center justify-between py-5 text-background_alt"
+      class="flex items-center justify-between container mx-auto relative h-full px-4"
     >
-      <NuxtLink :to="{name: 'index'}" class="flex space-x-1 text-xl items-center px-3">
+      <div class="flex items-center space-x-1">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           class="h-6 w-6"
@@ -54,21 +20,36 @@
             d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
           />
         </svg>
-        <span class="font-semibold uppercase">vivekascoder</span>
-      </NuxtLink>
-      <svg xmlns="http://www.w3.org/2000/svg" class="md:hidden h-6 w-6 box-content p-2 cursor-pointer hover:bg-gray-200 rounded-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" @click="toggleMenu()">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
-      </svg>
-      <div :class="{'hidden': showMenu}" class="absolute overflow-hidden md:static md:block bg-white shadow-md md:shadow-none left-0 right-0 top-full flex flex-col md:flex-row items-center links">
-        <span 
-          class="link p-4"
-          v-for="link in links"
-          :key="link.id"
+        <span class="font-semibold uppercase text-2xl">vivekascoder</span>
+      </div>
+      <div class="flex space-x-4 items-center h-full">
+        <div
+          class="flex md:space-x-4 md:items-center absolute md:static top-full bg-white md:bg-transparent flex-col md:flex-row left-0 right-0 md:mt-0 shadow-lg md:shadow-none md:border-none border-b-4 border-green-600 rounded-b-lg z-40"
+          :class="{'hidden md:static': showMenu}"
         >
-          <a href="#!">{{link.name}}</a>
-        </span>
-        <span class="pr-4 pl-2">|</span>
-        <span class="">
+          <span
+            class="my-1 px-4 py-4 text-center md:text-left md:py-1 hover:bg-gray-800 hover:text-white cursor-pointer md:rounded-full transition duration-100 ease-in-out block "
+            >Home</span
+          >
+          <span
+            class="my-1 px-4 py-4 text-center md:text-left md:py-1 hover:bg-gray-800 hover:text-white cursor-pointer md:rounded-full transition duration-100 ease-in-out block "
+            >Projects</span
+          >
+          <span
+            class="my-1 px-4 py-4 text-center md:text-left md:py-1 hover:bg-gray-800 hover:text-white cursor-pointer md:rounded-full transition duration-100 ease-in-out block "
+            >About Me</span
+          >
+          <span
+            class="my-1 px-4 py-4 text-center md:text-left md:py-1 hover:bg-gray-800 hover:text-white cursor-pointer md:rounded-full transition duration-100 ease-in-out block "
+            >Contact</span
+          >
+        </div>
+
+        <!-- Youtube Btn -->
+        <a
+          href="#!"
+          class="flex bg-red-500 items-center text-white px-6 py-2 rounded-full space-x-2 hover:bg-red-600 transition duration-100 ease-in-out"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             class="h-6 w-6"
@@ -80,7 +61,33 @@
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
-              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+              d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span class="font-semibold uppercase text-xs tracking-wider">Watch</span>
+        </a>
+        <span
+          class="static md:hidden bg-gray-100 p-3 rounded-full cursor-pointer hover:bg-gray-800 hover:text-gray-200 transition duration-100 ease-in-out"
+          @click="toggleMenu()"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16m-7 6h7"
             />
           </svg>
         </span>
@@ -97,25 +104,26 @@ export default {
       links: [
         {
           id: 1,
-          name: 'Home',
-          path: '/'
+          name: "Home",
+          path: "/"
         },
         {
           id: 2,
-          name: 'About',
-          path: '/about'
+          name: "About",
+          path: "/about"
         },
         {
           id: 3,
-          name: 'Contact',
-          path: '/'
+          name: "Contact",
+          path: "/"
         }
       ]
-    }
+    };
   },
   methods: {
     toggleMenu() {
-      this.showMenu = !this.showMenu
+      this.showMenu = !this.showMenu;
+      console.log(this.showMenu);
     }
   }
 };
